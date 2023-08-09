99369b0b98ea9043f4f787c682ef7e64c1f74151
Eric Arnhem <arnhemeric60@gmail.com>
2023-08-09 23:08:55 +0200

    Add i18n

--- src/main.js
+++ src/main.js
@@ -4,11 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/css/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
+
+const i18n = createI18n({
+  legacy: false, // Set to 'false' to use Composition API
+  locale: 'fr',
+  fallbackLocale: 'en'
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
