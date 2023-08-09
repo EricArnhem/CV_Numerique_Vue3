a18afe54501b0da569d8a19c0521e19bd25c65ec
Eric Arnhem <arnhemeric60@gmail.com>
2023-08-09 14:45:02 +0200

    Add content to Home view + move style into it
    Components commented because not implemented yet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,60 @@
 <script setup>
+
+const currentYear = () => {
+  let date = new Date();
+  return date.getFullYear();
+}
+
 </script>
 
 <template>
-  <main>
-    <p>Home</p>
-  </main>
+  <!-- <NavigationBar /> -->
+  <div id="main-container" class="container-fluid mt-md-5">
+    <div id="main-row" class="row justify-content-center mx-auto">
+
+      <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-4 col-xxl-3">
+        
+        <!-- <InfosBlock /> -->
+        <!-- <SkillsBlock /> -->
+        <!-- <HobbiesBlock /> -->
+
+       </div>
+
+       <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-6">
+         
+        <!-- <DescriptionBlock /> -->
+         <!-- <TrainingBlock /> -->
+         <!-- <ExperiencesBlock /> -->
+      </div>
+       <div class="row mt-5 justify-content-center">
+         <footer class="col-10 col-sm-8 col-lg-6 py-4 text-center">
+           <span>© <span id="current-year">{{ currentYear() }}</span> Eric Arnhem.</span>
+         </footer>
+       </div>
+       
+    </div>
+  </div>
+
 </template>
+
+<style>
+#main-row {
+  max-width: 2000px;
+}
+
+/* For xs and sm displays */
+@media (max-width: 767px) {
+
+  /* Removes the horizontal padding on the container and the row */
+  #main-container,
+  #main-row>* {
+    padding-left: 0;
+    padding-right: 0;
+  }
+
+}
+
+footer {
+  border-top: 1px solid rgb(222 226 230 / 36%);
+}
+</style>
