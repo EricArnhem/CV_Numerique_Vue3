f53519cbedecfe54e20774e4d209c4da62ab9190
Eric Arnhem <arnhemeric60@gmail.com>
2023-08-10 02:12:18 +0200

    Add Font Awesome icons

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import './assets/css/main.css'
 
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
+
+/* i18n */
+import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
   legacy: false, // Set to 'false' to use Composition API
@@ -14,9 +16,24 @@ const i18n = createI18n({
   fallbackLocale: 'en'
 })
 
+/* Font Awesome */
+// import the fontawesome core
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import font awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import specific icons
+import { faLanguage } from '@fortawesome/free-solid-svg-icons'
+
+// add icons to the library
+library.add(faLanguage)
+
 const app = createApp(App)
 
 app.use(router)
 app.use(i18n)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
